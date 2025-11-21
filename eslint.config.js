3674e5640ee79eb0384b4b63d29173afc59a5067
ikrambenallali <ikrambenallali2003@gmail.com>
2025-11-21 16:57:56 +0100

    fixe

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const { defineConfig } = require('eslint/config');
+
+module.exports = defineConfig([
+  {
+    files: ['/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^(next|error||sellerId)',
+          varsIgnorePattern:
+            '^(|coupon|productFactory|uri|Order|outputFormat)',
+        },
+      ],
+      'no-unexpected-multiline': 'off',
+    },
+  },
+  {
+    files: ['test//.js', '**/.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'off',
+    },
+  },
+]);
